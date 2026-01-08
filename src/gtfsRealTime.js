ea16820c2cd65f654910c9f7364fdd99b8cdf0c9
Daniel Valencia <dvalencia@maasapp.com>
2026-01-08 14:03:30 -0500

    feat: Enhance GTFS real-time data fetching with entity processing and file output

--- src/gtfsRealTime.js
+++ src/gtfsRealTime.js
@@ -1,5 +1,6 @@
 const GtfsRealtimeBindings = require("gtfs-realtime-bindings-transit");
 const axios = require("axios");
+const fs = require("fs");
 
 const requestSettings = {
   method: "GET",
@@ -9,16 +10,36 @@ const requestSettings = {
 
 async function fetchGtfsRealtimeData() {
   try {
+    const initTime = new Date();
     const response = await axios(requestSettings);
+
+    const endTime = new Date();
     // console.log("Response received", response.data);
-    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
+    let feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(
       response.data
     );
-    console.log("Feed Header:", feed.header);
-    console.log("Number of Entities:", feed.entity.length);
-    feed.entity.forEach((entity) => {
-      console.log("Entity ID:", entity);
+    feed = JSON.parse(JSON.stringify(feed));
+    // console.log("Feed Header:", feed.header);
+    // console.log("Number of Entities:", feed.entity.length);
+    const entities = feed.entity.map((entity) => {
+      if (entity.vehicle?.position) {
+        entity.vehicle.position.full = `${entity.vehicle.position.latitude},${entity.vehicle.position.longitude}`;
+      }
+      if (entity.vehicle.timestamp)
+        entity.vehicle.timestamptransformed = new Date(
+          entity.vehicle.timestamp * 1000 - 5 * 60 * 60 * 1000
+        ).toISOString();
+      entity.vehicle.initTime = initTime.toISOString();
+      entity.vehicle.endTime = endTime.toISOString();
+      return entity;
     });
+    // write entities to a file
+    fs.writeFileSync(
+      "./files/gtfs_realtime_entities.json",
+      JSON.stringify(entities, null, 2)
+    );
+
+    console.log("Entities written to gtfs_realtime_entities.json");
   } catch (error) {
     console.error(error);
   }
